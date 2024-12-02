eeb889c4a3f3900f580f983a50ab5329accc9fa5
gui-hmm <henriquemelomoura@gmail.com>
2024-12-02 10:56:48 -0300

    first commit

--- src/styled.ts
+++ src/styled.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     height: 100vh;
-    background-color: #EAF4FF;
+    background-color: #EBF4FF;
 `;
 
 export const Title = styled.h1`
